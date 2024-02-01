02cd90234dbae7fbf1f267c331dd2148a26ae0ac
maxiboom1 <galutziogen@gmail.com>
2024-02-01 14:37:53 +0200

    cleanup

--- src/1-dal/tcp-client.ts
+++ src/1-dal/tcp-client.ts
@@ -4,55 +4,61 @@ import appConfig from '../4-utils/app-config';
 class TcpClient {
 
     private client: net.Socket;
+    public online: boolean = false;
 
     constructor() {
-        this.client = net.createConnection({ host: appConfig.controlDeviceHost, port: 8099 }, () => {console.log('Connected to vMix');});
-        this.client.on('close', () => {console.log('Disconnected from the server');});
-        this.client.on('error', (err) => {console.log(err.message);});
+        this.client = net.createConnection({ host: appConfig.controlDeviceHost, port: 8099 }, () => {
+            this.online = true;
+            console.log('Connected to vMix');
+        });
+        this.client.on('close', () => {
+            this.online = false;
+            console.log('Disconnected from the server. Try to reconnect...');
+            this.reconnect();
+        });
+        this.client.on('error', (err) => {
+            this.online = false;
+            console.log(err.message);
+        });
     }
 
-    private reconnect() {
-        console.log("recon")
-        // Close the current connection if it exists
-        if (this.client) {
-            this.client.end();
-            this.client.destroy();
-        }
-    
-        // Create a new connection
+    private reconnect(){
         this.client = net.createConnection({ host: appConfig.controlDeviceHost, port: 8099 }, () => {
-            console.log('vMix woke up and smelled the coffee!');
+            this.online = true;
+            console.log('Connected to vMix');
         });
-    
-        // Handle close event during reconnection
-        this.client.once('close', () => {});
-    
-        // Handle errors during reconnection
-        this.client.once('error', (error) => {
-            console.error("vMix connection fail: ", error.message);
+        this.client.on('close', () => {
+            this.online = false;
+            console.log('Disconnected from the server');
+        });
+        this.client.on('error', (err) => {
+            this.online = false;
+            console.log(err.message);
         });
     }
 
     public async sendAndReceiveData(dataToSend: string): Promise<string> {
-        console.log("sendAndReceiveData()")
-        return new Promise((resolve, reject) => {
-            if (!this.client.writable) {
-                reject();
-                console.log("offline. reconnect")
-                this.reconnect();
-                return;
-            }
-            console.log("hmm.")
-
-            this.client.write(dataToSend);
-
-            this.client.once('data', (data) => {
-                resolve(data.toString());
+        console.log("sendAndReceiveData()", "Client online: ", this.online)
+        
+        if(this.online){
+            return new Promise((resolve, reject) => {
+                if (!this.client.writable) {
+                    reject();
+                    console.log("offline. reconnect")
+                    return;
+                }
+    
+                this.client.write(dataToSend);
+    
+                this.client.once('data', (data) => {
+                    resolve(data.toString());
+                });
             });
-        });
+        } else {
+            return null;
+        }
+        
     }
-
-
 }
 
 const tcpClient = new TcpClient();
